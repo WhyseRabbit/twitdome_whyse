6207cf3aa25cb5b239c9c344798dbe1eef0d4543
WhyseRabbit <whyse@centaur.com>
2020-12-02 11:07:45 -0700

    updated app, added db_model

--- Twitdome/app.py
+++ Twitdome/app.py
@@ -1,12 +1,24 @@
 from flask import Flask
+from .db_model import DB, User
 
 
 def create_app():
     """Create and configure Flask app."""
     app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///C:\\Users\\whyse\\Lambda Code\\Twitdome_Whyse\\Twitdome.sqlite3"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    DB.init_app(app)
 
     @app.route("/")
-
     def root():
         return "Hello, Twitter Dome!"
+    
+    @app.route("/<username>/<followers>")
+    """Adds User to User-class DataBase."""
+    def add_user(username, followers):
+        user = User(username=username, followers=followers)
+        DB.session.add(user)
+        DB.session.commit()
+        return f"{username} has entered the Dome!"
+
     return app
