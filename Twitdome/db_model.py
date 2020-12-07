84799765e30965753caf70c8046cca945b1243b2
WhyseRabbit <whyse@centaur.com>
2020-12-06 17:34:04 -0700

    init update

--- Twitdome/db_model.py
+++ Twitdome/db_model.py
@@ -21,15 +21,15 @@ class Tweet(DB.Model):
     def __repr__(self):
         return "<Tweet %r>" % self.text
 
-flask shell
->>> from Twitdome.db_model import DB, User, Tweet
->>> DB.create_all()
->>> user1 = User(username="BarackObama")
->>> user2 = User(username="1JimmyTheRabbit")
->>> tweet1 = Tweet(text="President, White House, and The United States of America.")
->>> tweet2 = Tweet(text="The Secret is coming for you...")
->>> user1.text.append(tweet1)
->>> user2.text.append(tweet2)
->>> DB.session.add(user1)
->>> DB.session.add(user2)
->>> DB.session.commit()
+# flask shell
+# >>> from Twitdome.db_model import DB, User, Tweet
+# >>> DB.create_all()
+# >>> user1 = User(username="BarackObama")
+# >>> user2 = User(username="1JimmyTheRabbit")
+# >>> tweet1 = Tweet(text="President, White House, and The United States of America.")
+# >>> tweet2 = Tweet(text="The Secret is coming for you...")
+# >>> user1.text.append(tweet1)
+# >>> user2.text.append(tweet2)
+# >>> DB.session.add(user1)
+# >>> DB.session.add(user2)
+# >>> DB.session.commit()
