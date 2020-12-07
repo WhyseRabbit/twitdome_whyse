29e05e0273e0dbf0b30215b1249edb727fd9463c
WhyseRabbit <whyse@centaur.com>
2020-12-06 20:11:50 -0700

    db_model tweets column added to User class

--- Twitdome/db_model.py
+++ Twitdome/db_model.py
@@ -7,6 +7,8 @@ DB = SQLAlchemy()
 class User(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True)
     username = DB.Column(DB.String(80), unique=True, nullable=False)
+    tweet_id = DB.Column(DB.Integer, DB.ForeignKey("tweet.id"), nullable=False)
+    tweets = DB.relationship("Tweet", backref=DB.backref("username", lazy=True))
 
     def __repr__(self):
         return "<User %r>" % self.username
@@ -16,7 +18,7 @@ class Tweet(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True)
     text = DB.Column(DB.String(280), unique=True, nullable=False)
     user_id = DB.Column(DB.Integer, DB.ForeignKey("user.id"), nullable=False)
-    user = DB.relationship("User", backref=DB.backref("tweet", lazy=True))
+    user = DB.relationship("User", backref=DB.backref("text", lazy=True))
 
     def __repr__(self):
         return "<Tweet %r>" % self.text
