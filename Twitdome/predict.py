b3e82040222c5ac36e65f2f6ccbe3a733276181d
WhyseRabbit <whyse@centaur.com>
2020-12-07 20:42:09 -0700

    variable update

--- Twitdome/predict.py
+++ Twitdome/predict.py
@@ -8,8 +8,8 @@ def predict_user(user1, user2, tweet_text):
     """Predicts which user is most likely to say what."""
     user1 = User.query.filter(User.username == user1).one()
     user2 = User.query.filter(User.username == user2).one()
-    user1_embed = np.array([tweet.embedding for tweet in user1.tweet])
-    user2_embed = np.array([tweet.embedding for tweet in user2.tweet])
+    user1_embed = np.array([tweet.embedding for tweet in user1.tweets])
+    user2_embed = np.array([tweet.embedding for tweet in user2.tweets])
 
     embeds = np.vstack([user1_embed, user2_embed])
     labels = np.concatenate([np.ones(len(user1_embed)),
