70327f055dbe60bfda8c8084495443939a784627
WhyseRabbit <whyse@centaur.com>
2020-12-07 20:15:46 -0700

    enabled spacy

--- Twitdome/twitter.py
+++ Twitdome/twitter.py
@@ -16,10 +16,10 @@ TWITTER_AUTH = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_SECRET_API)
 TWITTER_AUTH.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
 TWITTER = tweepy.API(TWITTER_AUTH)
 
-# nlp = spacy.load("en_core_web_sm", disable=["tagger", "parser"])
+nlp = spacy.load("en_core_web_sm", disable=["tagger", "parser"])
 
-# def tweet_vector(nlp, tweet_text):
-#     return list(nlp(tweet_text).vector)
+def tweet_vector(nlp, tweet_text):
+    return list(nlp(tweet_text).vector)
 
 def add_user_tweepy(username):
     """Add a user and their tweets to database"""
@@ -42,15 +42,15 @@ def add_user_tweepy(username):
             db_user.newest_tweet_id = tweets[0].id
             pass
 
-        # for tweet in tweets:
+        for tweet in tweets:
 
-        #     embedding = tweet_vector(nlp, tweet.full_text)
+            embedding = tweet_vector(nlp, tweet.full_text)
 
-        #     db_tweet = Tweet(id=tweet.id,
-        #                      tweet=tweet.full_text[:300],
-        #                      embedding=embedding)
-        #     db_user.tweet.append(db_tweet)
-        #     DB.session.add(db_tweet)
+            db_tweet = Tweet(id=tweet.id,
+                             tweet=tweet.full_text[:300],
+                             embedding=embedding)
+            db_user.tweet.append(db_tweet)
+            DB.session.add(db_tweet)
 
     except Exception as e:
         print('Error processing {}: {}'.format(username, e))
@@ -97,14 +97,14 @@ def add_user_history(username):
 
         print(f"Total Tweets collected for {username}: {len(tweet_history)}")
 
-        # for tweet in tweet_history:
-        #     embedding = tweet_vector(nlp, tweet.full_text)
+        for tweet in tweet_history:
+            embedding = tweet_vector(nlp, tweet.full_text)
 
-        #     db_tweet = Tweet(id=tweet.id,
-        #                      tweet=tweet.full_text[:300],
-        #                      embedding=embedding)
-        #     db_user.tweet.append(db_tweet)
-        #     DB.session.add(db_tweet)
+            db_tweet = Tweet(id=tweet.id,
+                             tweet=tweet.full_text[:300],
+                             embedding=embedding)
+            db_user.tweet.append(db_tweet)
+            DB.session.add(db_tweet)
 
     except Exception as e:
         print(f"Error processing {username}: {e}")
