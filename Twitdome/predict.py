18618850df1ec0e32e6d745965c95a61ca2255a6
WhyseRabbit <whyse@centaur.com>
2020-12-09 17:10:39 -0700

    predict glitch update

--- Twitdome/predict.py
+++ Twitdome/predict.py
@@ -7,8 +7,8 @@ from .twitter import nlp, tweet_vector
 def predict_user(user1, user2, tweet_text):
     user1 = User.query.filter(User.username == user1).one()
     user2 = User.query.filter(User.username == user2).one()
-    user1_embed = np.array([tweet.embed for tweet in user1.tweets])
-    user2_embed = np.array([tweet.embed for tweet in user2.tweets])
+    user1_embed = np.array([tweets.embed for tweets in user1.tweets])
+    user2_embed = np.array([tweets.embed for tweets in user2.tweets])
 
     embeds = np.vstack([user1_embed, user2_embed])
     labels = np.concatenate([np.ones(len(user1_embed)),
