216c64f1ad39ab9a76c5699e89a782af9783c4ff
WhyseRabbit <whyse@centaur.com>
2020-12-09 18:05:35 -0700

    predict tweak

--- Twitdome/predict.py
+++ Twitdome/predict.py
@@ -1,6 +1,6 @@
 import pickle
 import numpy as np
-from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
 from .db_model import User
 from .twitter import nlp, tweet_vector
 
@@ -17,7 +17,7 @@ def predict_user(user_1, user_2, tweet_text):
     labels = np.concatenate([np.ones(len(user1.tweets)),
                              np.zeros(len(user2.tweets))])
     breakpoint()
-    log_reg = LogisticRegression(max_iter=1000).fit(embeds, labels)
+    knc = KNeighborsClassifier(weights='distance', metric='cosine').fit(embeds, labels)
     embed_tweet = tweet_vector(tweet_text=tweet_text)
     breakpoint()
-    return log_reg.predict(np.array(embed_tweet).reshape(1, -1))
+    return knc.predict(np.array(embed_tweet).reshape(1, -1))
