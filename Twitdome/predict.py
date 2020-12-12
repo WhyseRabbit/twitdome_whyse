d2f395144f263b30e6c1b311795fbcddff0b9050
WhyseRabbit <aae@centaur.me>
2020-12-11 17:48:26 -0700

    removed breakpoints

--- Twitdome/predict.py
+++ Twitdome/predict.py
@@ -7,15 +7,15 @@ from .twitter import nlp, tweet_vector
 def predict_user(user_1, user_2, tweet_text):
     user1 = User.query.filter(User.username == user_1).one()
     user2 = User.query.filter(User.username == user_2).one()
-    breakpoint()
+
     user1_embed = np.array([tweet.embed for tweet in user1.tweet])
     user2_embed = np.array([tweet.embed for tweet in user2.tweet])
-    breakpoint()
+
     embeds = np.vstack([user1_embed, user2_embed])
     labels = np.concatenate([np.ones(len(user1.tweets)),
                              np.zeros(len(user2.tweets))])
-    breakpoint()
+
     knc = KNeighborsClassifier(weights='distance', metric='cosine').fit(embeds, labels)
     embed_tweet = tweet_vector(tweet_text=tweet_text)
-    breakpoint()
+
     return knc.predict([embed_tweet])[0]
